c04a8a487ffd2347a644e04905779855cc3f5b65
Kenneth Yu <kenyu@ucdavis.edu>
2013-12-04 00:49:41 -0800

    removed everything

--- README.md
+++
@@ -1,5 +0,0 @@
-# Ken's Portfolio Website
-
-I am creating a portfolio website to display my past projects.
-
-### WIP
--- index.js
+++
@@ -1,7 +0,0 @@
-attachListeners = function() {
-  $('button#about-me').on('click', function() {
-    $('#area-to-populate').append(ABOUTME)
-  })
-}
-
-$(document).ready(attachListeners)
