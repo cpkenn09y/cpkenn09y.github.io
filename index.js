06d100f61c81b2f599a28775a306fd1b1fb57db7
Kenneth Yu <kenyu@ucdavis.edu>
2013-12-06 00:39:57 -0800

    guest can click on navigation numbers to navigate through projects
    * created a new div called nav-projects to contain the navigation numbers
    * edited css to color coat each number
    * the active number is highlighted

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ attachListeners = function() {
 }
 
 animateProjectImages = function() {
-  $('ul.project-images').cycle({fx: 'shuffle', speed: 800, timeout: 5000, pause: 1})
+  $('ul.project-images').before("<div id='nav-projects'></div>").cycle({fx: 'shuffle', speed: 800, timeout: 5000, pause: 1, pager: '#nav-projects'})
 }
 
 setUpApplication = function() {
