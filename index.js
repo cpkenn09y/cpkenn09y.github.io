0c8d30c87bd0d5d79ec31cc5bbc4703c02dc9ba8
Kenneth Yu <kenyu@ucdavis.edu>
2013-12-06 01:36:30 -0800

    refactored the animateProjectImages function
    * more readable

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ attachListeners = function() {
 }
 
 animateProjectImages = function() {
-  $('ul.project-images').before("<div id='nav-projects'></div>").cycle({fx: 'shuffle', speed: 800, timeout: 5000, pause: 1, pager: '#nav-projects'})
+  $('ul.project-images').before("<div id='nav-projects'></div>").cycle({
+    fx: 'shuffle',
+    speed: 800,
+    timeout: 5000,
+    pause: 1,
+    pager: '#nav-projects'
+  })
 }
 
 setUpApplication = function() {
